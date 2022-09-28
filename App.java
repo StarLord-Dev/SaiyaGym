60401f9ca0815ba19a63799135f86b48a6d11d26
StarLord-Dev <88408516+StarLord-Dev@users.noreply.github.com>
2022-09-28 10:40:51 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+package StartUp;
+
+import Connective.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Connective conn = new Connective();
+
+    }
+}
